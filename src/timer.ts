6e363a654644814916eee3e5cdcd93a7fef23339
Skaytacium <skaytacium@gmail.com>
2021-01-09 21:16:11 +0530

    Tick tock
    Round goes the clock, while bio struggles, and the code crumbles, I make
    stuff which reloads the glock.
    
    Help

--- src/timer.ts
+++ src/timer.ts
@@ -1,33 +1,13 @@
 import { EventEmitter } from 'events';
 
-export class Timer extends Promise<undefined>{
-    id: string;
-    time;
-
-    constructor(
-        time: number,
-        id: string,
-    ) {
-        super((res) => { Timer.sleep(time); res(undefined); });
-
-        this.time = time;
-        this.id = id;
-        console.log(`Created a new timer with time ${time} and id ${id}`);
-    }
-
-    static sleep(ms: number) {
-        const date = Date.now();
-        let future = date + ms;
-
-        while (future - date > 0)
-            future = Date.now();
-
-        return true;
-    }
+function timer(time: number) {
+    return new Promise(res => {
+        setTimeout(res, time);
+    });
 }
 
 export class Plate extends EventEmitter {
-    readonly timers: { [id: string]: Timer } = {};
+    readonly timers: { [id: string]: Promise<any> } = {};
 
     constructor() {
         super()
@@ -35,19 +15,31 @@ export class Plate extends EventEmitter {
         console.log("Created a new Plate.");
     }
 
-    add(id: string, time: number) {
-        this.timers[id] = new Timer(time, id);
+    public add(id: string, time: number) {
+        this.timers[id] = timer(time);
         this.emit('add', [id, time]);
-        console.log("Added a new timer with id " + id + " and time " + time); //I felt like using +
+
+        this.timers[id].then(() => { this.done(id) });
+
+        console.log("Added a new timer with ID " + id + " and time " + time); //I felt like using +
     }
 
-    del(id: string) {
+    private done(id: string) {
+        console.log("Timer with ID " + id + " is done");
+        this.emit('fin', id);
+    }
+
+    public del(id: string) {
         if (this.timers[id]) {
             delete this.timers[id];
             this.emit('del', id);
+
             console.log("Deleted timer " + id);
+
+            return true;
         } else {
-            console.error("Couldn't find timer with id " + id);
+            console.error("Couldn't find timer with ID " + id);
+            return false;
         }
     }
 }
